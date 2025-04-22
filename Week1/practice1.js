44b59c549ea2fbf153f11d0fd585c109f9e6e0ac
Jamison <j.jpurcell593@protonmail.com>
2025-04-22 15:08:29 -0600

    week 1

---
+++ Week1/practice1.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 2;
+let toal = myApples = friendApples;
+
+document.getElementById("myApplesElemet").textContent = myApples;
+document.getElementById("friendApplesElemet").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
